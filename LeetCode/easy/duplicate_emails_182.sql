79f20ae46c4b94572a0c8d40be38eeb5819c64c0
Peyton Bechard <pbechard@my.yorku.ca>
2022-02-27 15:14:07 +0800

    reformat MySQL Leetcode problem 182

--- LeetCode/easy/duplicate_emails_182.sql
+++ LeetCode/easy/duplicate_emails_182.sql
@@ -1,13 +1,11 @@
-"""
+/*
 182. Duplicate Emails
 https://leetcode.com/problems/duplicate-emails/
-"""
+*/
 
-# MySQL
 
-"""
 SELECT email
 FROM Person
 GROUP BY email
 HAVING COUNT(email) > 1;
-"""
+
