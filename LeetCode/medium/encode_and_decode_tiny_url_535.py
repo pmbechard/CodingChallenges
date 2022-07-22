460193c43de28ad18dc00c30e6a1146bced72ef9
Peyton Bechard <pbechard@my.yorku.ca>
2022-07-22 20:28:34 +0800

    Adds LeetCode problem 535

---
+++ LeetCode/medium/encode_and_decode_tiny_url_535.py
@@ -0,0 +1,17 @@
+"""
+535. Encode and Decode TinyURL
+https://leetcode.com/problems/encode-and-decode-tinyurl/
+"""
+
+
+class Codec:
+
+    def encode(self, longUrl: str) -> str:
+        return longUrl
+
+    def decode(self, shortUrl: str) -> str:
+        return shortUrl
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
