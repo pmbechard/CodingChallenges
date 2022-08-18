d98ed4e8535e178620180e10c77691fc428feb0c
Peyton Bechard <pbechard@my.yorku.ca>
2022-08-18 11:42:01 +0800

    Adds LeetCode problem 1534

---
+++ LeetCode/easy/count_good_triplets_1534.py
@@ -0,0 +1,25 @@
+"""
+1534. Count Good Triplets
+https://leetcode.com/problems/count-good-triplets/
+"""
+
+
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        l = 0
+        r = len(arr) - 1
+        minimum = min(a, b, c)
+        triplets_indices = []
+
+        while l <= len(arr) - 3:
+            if abs(arr[l] - arr[r]) <= c:
+                for i in arr[ l +1:r]:
+                    if abs(arr[r] - i) <= b and abs(arr[l] - i) <= a:
+                        triplets_indices.append([l, '-', r])
+            if len(arr[l: r +1]) == 3:
+                l += 1
+                r = len(arr) - 1
+            else:
+                r -= 1
+
+        return len(triplets_indices)
