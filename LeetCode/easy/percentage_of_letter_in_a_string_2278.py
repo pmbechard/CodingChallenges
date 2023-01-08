4a81ac91f23c0b963a427c139e384e66f8d80314
Peyton Bechard <pbechard@my.yorku.ca>
2023-01-08 18:23:56 +0800

    Adds LeetCode problem 2278

---
+++ LeetCode/easy/percentage_of_letter_in_a_string_2278.py
@@ -0,0 +1,9 @@
+"""
+2278. Percentage of Letter in String
+https://leetcode.com/problems/percentage-of-letter-in-string/
+"""
+
+
+class Solution:
+    def percentageLetter(self, s: str, letter: str) -> int:
+        return int(s.count(letter) / len(s) * 100)
