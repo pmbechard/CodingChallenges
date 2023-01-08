f5ee2fa88f7c405226f3abb186c5204c249cf8e1
Peyton Bechard <pbechard@my.yorku.ca>
2023-01-08 16:18:23 +0800

    Adds LeetCode problem 1381

---
+++ LeetCode/medium/design_a_stack_with_an_increment_operation_1381.py
@@ -0,0 +1,23 @@
+"""
+1381. Design a Stack With Increment Operation
+https://leetcode.com/problems/design-a-stack-with-increment-operation/
+"""
+
+
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.maxSize = maxSize
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.maxSize:
+            self.stack.append(x)
+
+    def pop(self) -> int:
+        return self.stack.pop() if self.stack else -1
+
+    def increment(self, k: int, val: int) -> None:
+        k = min(k, len(self.stack))
+        for i in range(k):
+            self.stack[i] += val
