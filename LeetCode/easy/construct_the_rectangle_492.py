4e3d3767237b92e50f6858f5d0c4d326f52e43c7
Peyton Bechard <pbechard@my.yorku.ca>
2023-01-14 16:37:24 +0800

    Adds LeetCode problem 492

---
+++ LeetCode/easy/construct_the_rectangle_492.py
@@ -0,0 +1,11 @@
+"""
+492. Construct the Rectangle
+https://leetcode.com/problems/construct-the-rectangle/
+"""
+
+
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        for i in range(int(area**0.5), 0, -1):
+            if area % i == 0:
+                return [area // i, i]
