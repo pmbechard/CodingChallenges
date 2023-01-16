3e56be386b9caf41da4bc190bd3ab3bb56543f08
Peyton Bechard <pbechard@my.yorku.ca>
2023-01-16 11:27:55 +0800

    Adds LeetCode problem 349

---
+++ LeetCode/easy/intersection_of_two_arrays_349.py
@@ -0,0 +1,9 @@
+"""
+349. Intersection of Two Arrays
+https://leetcode.com/problems/intersection-of-two-arrays/
+"""
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return set(nums1).intersection(nums2)
