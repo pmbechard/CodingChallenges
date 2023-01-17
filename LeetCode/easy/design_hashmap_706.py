0d144f89fcf8cae2a6e77b1942cf5cf13f8b0b86
Peyton Bechard <pbechard@my.yorku.ca>
2023-01-17 09:25:10 +0800

    Updates LeetCode problem 706

--- LeetCode/easy/design_hashmap_706.py
+++ LeetCode/easy/design_hashmap_706.py
@@ -7,29 +7,17 @@ https://leetcode.com/problems/design-hashmap/description/
 class MyHashMap:
 
     def __init__(self):
-        self.map = []
+        self.map = [None] * (10 ** 6 + 1)
 
     def put(self, key: int, value: int) -> None:
-        for i in self.map:
-            if i[0] == key:
-                i[1] = value
-                return
-        self.map.append([key, value])
+        self.map[key] = value
 
     def get(self, key: int) -> int:
-        for i in self.map:
-            if i[0] == key:
-                return i[1]
-        return -1
+        val = self.map[key]
+        return val if val != None else -1
 
     def remove(self, key: int) -> None:
-        index = -1
-        for i in range(len(self.map)):
-            if self.map[i][0] == key:
-                index = i
-                break
-        if index != -1:
-            self.map = self.map[:index] + self.map[index + 1:]
+        self.map[key] = None
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
