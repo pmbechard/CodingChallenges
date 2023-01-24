818024d9daf1a417cfb5fba363d33468746a49c1
Peyton Bechard <peyton.bechard@gmail.com>
2023-01-24 20:53:10 +0800

    Adds LeetCode problem 1492

---
+++ LeetCode/medium/the_kth_factor_of_n_1492.py
@@ -0,0 +1,12 @@
+"""
+1492. The kth Factor of n
+https://leetcode.com/problems/the-kth-factor-of-n/
+"""
+
+
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        for i in range(1, n + 1):
+            if n % i == 0: k -= 1
+            if k == 0: return i
+        return -1
