b13c4387e007a55f4074537dd56f5c8cb5436c69
Peyton Bechard <peyton.bechard@gmail.com>
2023-03-01 10:28:30 +0800

    Adds LeetCode problem 371

---
+++ LeetCode/medium/sum_of_two_integers_371.py
@@ -0,0 +1,9 @@
+"""
+371. Sum of Two Integers
+https://leetcode.com/problems/sum-of-two-integers/
+"""
+
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        return eval(f'{a} + {b}')
