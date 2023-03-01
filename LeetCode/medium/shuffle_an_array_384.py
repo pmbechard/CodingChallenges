ac5b1a7f971bc492ee39b28ace7ea9ac54e1bc81
Peyton Bechard <peyton.bechard@gmail.com>
2023-03-01 10:05:56 +0800

    Adds LeetCode problem 384

---
+++ LeetCode/medium/shuffle_an_array_384.py
@@ -0,0 +1,26 @@
+"""
+384. Shuffle an Array
+https://leetcode.com/problems/shuffle-an-array/
+"""
+
+import random
+
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.original = nums[:]
+
+    def reset(self) -> List[int]:
+        self.nums = self.original[:]
+        return self.nums
+
+    def shuffle(self) -> List[int]:
+        random.shuffle(self.nums)
+        return self.nums
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
