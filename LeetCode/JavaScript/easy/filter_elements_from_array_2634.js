1f899314b391597449bad383c28023ff0b73f35c
Peyton Bechard <peyton.bechard@gmail.com>
2023-04-12 19:41:01 +0800

    Adds LeetCode problem 2634

--- LeetCode/JavaScript/easy/filter_elements_from_array_2634.js
+++ LeetCode/JavaScript/easy/filter_elements_from_array_2634.js
@@ -10,5 +10,9 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    return arr.filter(fn)
+    const result = []
+    for (let i = 0; i < arr.length; i++)
+        if (fn(arr[i], i)) result.push(arr[i])
+    return result
 };
+
