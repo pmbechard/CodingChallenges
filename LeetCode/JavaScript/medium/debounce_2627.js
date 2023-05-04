c2fbebb1ba3ca5de2752b7289806826581b4ef48
Peyton Bechard <peyton.bechard@gmail.com>
2023-05-04 08:14:59 +0800

    Adds LeetCode problem 2627

---
+++ LeetCode/JavaScript/medium/debounce_2627.js
@@ -0,0 +1,27 @@
+/**
+* 2627. Debounce
+* https://leetcode.com/problems/debounce/
+*/
+
+
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    var timer = null;
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn(...args);
+        }, t);
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
