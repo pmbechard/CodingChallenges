feff1a71438bba01437ea0cc36d22a674f84efdb
Peyton Bechard <peyton.bechard@gmail.com>
2023-05-22 11:11:46 +0800

    Adds LeetCode problem 2665

---
+++ LeetCode/JavaScript/easy/counter_ii_2665.js
@@ -0,0 +1,24 @@
+/**
+* 2665. Counter II
+* https://leetcode.com/problems/counter-ii/
+*/
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init;
+    return {
+        increment: () => ++count,
+        decrement: () => --count,
+        reset: () => (count = init),
+    };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
