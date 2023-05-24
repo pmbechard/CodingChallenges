cce8318cc28d4942152b9be71ae7d59d19de5a16
Peyton Bechard <peyton.bechard@gmail.com>
2023-05-24 09:35:41 +0800

    Adds LeetCode problem 2648

---
+++ LeetCode/JavaScript/easy/generate_fibonacci_sequence_2648.js
@@ -0,0 +1,27 @@
+/**
+* 2648. Generate Fibonacci Sequence
+* https://leetcode.com/problems/generate-fibonacci-sequence/
+*/
+
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let prev = 0;
+    let current = 1
+    yield 0;
+    yield 1;
+
+    while (true) {
+        temp = current;
+        current = prev + current;
+        prev = temp;
+        yield current;
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
