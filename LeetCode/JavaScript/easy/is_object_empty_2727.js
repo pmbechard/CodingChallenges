39a3024043109a972a9ef58abe0523e580d7fb7c
Peyton Bechard <peyton.bechard@gmail.com>
2023-06-11 16:52:09 +0800

    Adds LeetCode problem 2727

---
+++ LeetCode/JavaScript/easy/is_object_empty_2727.js
@@ -0,0 +1,13 @@
+/**
+* 2727. Is Object Empty
+* https://leetcode.com/problems/is-object-empty/
+*/
+
+
+/**
+ * @param {Object | Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    return Object.keys(obj).length === 0;
+};
