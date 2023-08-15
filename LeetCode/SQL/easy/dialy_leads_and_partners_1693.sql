46ddff603ddabce127e7f38d05fbcee2c2346bc8
Peyton Bechard <peyton.bechard@gmail.com>
2023-08-16 07:44:06 +0800

    Adds LeetCode problem 1693

---
+++ LeetCode/SQL/easy/dialy_leads_and_partners_1693.sql
@@ -0,0 +1,8 @@
+# 1693. Daily Leads and Partners
+# https://leetcode.com/problems/daily-leads-and-partners/
+
+
+# Write your MySQL query statement below
+SELECT date_id, make_name, COUNT(DISTINCT lead_id) as unique_leads, COUNT(DISTINCT partner_id) as unique_partners
+FROM DailySales
+GROUP BY date_id, make_name;
