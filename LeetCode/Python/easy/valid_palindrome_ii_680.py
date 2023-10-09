40e4d11f1ba373b25225391d6383331a31a6be1f
Peyton Bechard <peyton.bechard@gmail.com>
2023-10-09 11:14:44 +0800

    Adds LeetCode problem 680

---
+++ LeetCode/Python/easy/valid_palindrome_ii_680.py
@@ -0,0 +1,32 @@
+"""
+680. Valid Palindrome II
+https://leetcode.com/problems/valid-palindrome-ii/
+"""
+
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        if len(s) == 1:
+            return True
+        elif len(s) == 2:
+            return True
+        elif len(s) == 3:
+            return s[0] == s[1] or s[0] == s[2] or s[1] == s[2]
+        elif s == s[::-1]:
+            return True
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return self.is_palindrome(s[l + 1:r + 1]) or self.is_palindrome(s[l:r])
+            l += 1
+            r -= 1
+        return True
+
+    def is_palindrome(self, s):
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
