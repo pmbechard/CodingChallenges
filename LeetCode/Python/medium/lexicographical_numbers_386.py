76bc7a68a74c5461a5c7d63051beb5ccc810febd
Peyton Bechard <peyton.bechard@gmail.com>
2023-11-15 08:10:34 +0800

    Adds LeetCode problem 286

---
+++ LeetCode/Python/medium/lexicographical_numbers_386.py
@@ -0,0 +1,9 @@
+"""
+386. Lexicographical Numbers
+https://leetcode.com/problems/lexicographical-numbers/
+"""
+
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        return sorted(list(range(1, n + 1)), key=str)
