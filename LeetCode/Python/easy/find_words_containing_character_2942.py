41640d4a68d1a554fbb83a581fbbe12db76a28c6
Peyton Bechard <peyton.bechard@gmail.com>
2023-11-26 09:40:24 +0800

    Adds LeetCode problem 2942

---
+++ LeetCode/Python/easy/find_words_containing_character_2942.py
@@ -0,0 +1,9 @@
+"""
+2942. Find Words Containing Character
+https://leetcode.com/problems/find-words-containing-character/
+"""
+
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        return [i for i in range(len(words)) if x in words[i]]
