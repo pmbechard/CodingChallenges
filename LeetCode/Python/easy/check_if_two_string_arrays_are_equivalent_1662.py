902b0e127e12d028cc0fac7bc83de7d3b14e12c8
Peyton Bechard <peyton.bechard@gmail.com>
2023-12-01 10:11:41 +0800

    Updates LeetCode problem 1662

--- LeetCode/Python/easy/check_if_two_string_arrays_are_equivalent_1662.py
+++ LeetCode/Python/easy/check_if_two_string_arrays_are_equivalent_1662.py
@@ -6,12 +6,13 @@ https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
 
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        word1_str = ""
-        word2_str = ""
-        longest_arr = max(len(word1), len(word2))
-        for i in range(longest_arr):
-            if i < len(word1):
-                word1_str += word1[i]
-            if i < len(word2):
-                word2_str += word2[i]
-        return word1_str == word2_str
+        # word1_str = ""
+        # word2_str = ""
+        # longest_arr = max(len(word1), len(word2))
+        # for i in range(longest_arr):
+        #     if i < len(word1):
+        #         word1_str += word1[i]
+        #     if i < len(word2):
+        #         word2_str += word2[i]
+        # return word1_str == word2_str
+        return ''.join(word1) == ''.join(word2)
