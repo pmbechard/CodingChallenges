2a029e349737f7bed77c8b499f6c87e2a8329409
Peyton Bechard <peyton.bechard@gmail.com>
2023-12-05 15:00:49 +0800

    Updates LeetCode problem 1688

--- LeetCode/Python/easy/count_of_matches_in_tournament_1688.py
+++ LeetCode/Python/easy/count_of_matches_in_tournament_1688.py
@@ -6,12 +6,4 @@ https://leetcode.com/problems/count-of-matches-in-tournament/
 
 class Solution:
     def numberOfMatches(self, n: int) -> int:
-        total = 0
-        while n != 1:
-            if n % 2 == 0:
-                total += n//2
-                n = n//2
-            else:
-                total += (n-1)//2
-                n = (n-1)//2+1
-        return total
+        return n - 1
