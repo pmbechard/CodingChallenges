923d8f28e309d5110236bcaec873be8d7f5dda35
Peyton Bechard <peyton.bechard@gmail.com>
2023-12-15 08:13:10 +0800

    Updates LeetCode problem 1436

--- LeetCode/Python/easy/destination_city_1436.py
+++ LeetCode/Python/easy/destination_city_1436.py
@@ -6,12 +6,18 @@ https://leetcode.com/problems/destination-city/
 
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        from_to = [[], []]
-        for i in paths:
-            from_to[0].append(i[0])
-            from_to[1].append(i[1])
-        print(from_to)
+        # from_to = [[], []]
+        # for i in paths:
+        #     from_to[0].append(i[0])
+        #     from_to[1].append(i[1])
 
-        for i in from_to[1]:
-            if i not in from_to[0]:
-                return i
+        # for i in from_to[1]:
+        #     if i not in from_to[0]:
+        #         return i
+
+        dic = dict()
+        for path in paths:
+            dic[path[0]] = path[1]
+        for v in dic.values():
+            if not dic.get(v):
+                return v
