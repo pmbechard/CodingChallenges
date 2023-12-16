d51a083dab3953e132fa0a424f4baff454406183
Peyton Bechard <peyton.bechard@gmail.com>
2023-12-16 08:45:07 +0800

    Updates LeetCode problem 242

--- LeetCode/Python/easy/valid_anagram_242.py
+++ LeetCode/Python/easy/valid_anagram_242.py
@@ -6,4 +6,5 @@ https://leetcode.com/problems/valid-anagram/
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(list(s)) == sorted(list(t))
+        # return sorted(list(s)) == sorted(list(t))
+        return Counter(s) == Counter(t)
