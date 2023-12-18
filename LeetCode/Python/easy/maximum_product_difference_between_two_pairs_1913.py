757c4b208ef77f92e6db48818ac17a1adb59ec33
Peyton Bechard <peyton.bechard@gmail.com>
2023-12-18 09:18:44 +0800

    Updates LeetCode problem 1913

--- LeetCode/Python/easy/maximum_product_difference_between_two_pairs_1913.py
+++ LeetCode/Python/easy/maximum_product_difference_between_two_pairs_1913.py
@@ -3,6 +3,7 @@
 https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
 """
 
+
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
         lowest = nums.pop(nums.index(min(nums)))
@@ -10,3 +11,6 @@ class Solution:
         largest = nums.pop(nums.index(max(nums)))
         largest *= nums.pop(nums.index(max(nums)))
         return largest - lowest
+
+        # nums.sort()
+        # return nums[-1] * nums[-2] - nums[0] * nums[1]
