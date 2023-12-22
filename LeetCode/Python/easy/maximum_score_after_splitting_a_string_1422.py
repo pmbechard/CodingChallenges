68c51a17e32acf62e1d8483cedb6413b8a4cfa89
Peyton Bechard <peyton.bechard@gmail.com>
2023-12-22 09:06:12 +0800

    Updates LeetCode problem 1422

--- LeetCode/Python/easy/maximum_score_after_splitting_a_string_1422.py
+++ LeetCode/Python/easy/maximum_score_after_splitting_a_string_1422.py
@@ -3,12 +3,17 @@
 https://leetcode.com/problems/maximum-score-after-splitting-a-string/
 """
 
+
 class Solution:
     def maxScore(self, s: str) -> int:
-        total = 0
-        for i in range(1, len(s)):
-            current = s[:i].count('0') + s[i:].count('1')
-            if current > total:
-                total = current
+        l = 1 if s[0] == '0' else 0
+        r = s[1:].count('1')
+        total = r + l
+        for i in range(1, len(s) - 1):
+            if s[i] == '1':
+                r -= 1
+            else:
+                l += 1
+            if r + l > total:
+                total = r + l
         return total
-
