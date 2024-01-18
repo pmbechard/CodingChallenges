3479663961090df45a2410597c3b9542006fa88f
Peyton Bechard <peyton.bechard@gmail.com>
2024-01-18 10:18:07 +0800

    Updates LeetCode problem 70

--- LeetCode/Python/easy/climbing_stairs_70.py
+++ LeetCode/Python/easy/climbing_stairs_70.py
@@ -5,9 +5,15 @@ https://leetcode.com/problems/climbing-stairs/
 
 
 class Solution:
+    def __init__(self):
+        self.memo = dict()
+
     def climbStairs(self, n):
-        a, b = 1, 1
-        while n > 1:
-            a, b = b, a + b
-            n -= 1
-        return b
+        if n in self.memo:
+            return self.memo[n]
+        if n <= 3:
+            return n
+        output = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        if not n in self.memo:
+            self.memo[n] = output
+        return output
