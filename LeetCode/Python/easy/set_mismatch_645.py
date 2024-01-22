d4e907da60f90d1fe4f4c5fbe9db69a16919fb4d
Peyton Bechard <peyton.bechard@gmail.com>
2024-01-22 15:04:42 +0800

    Updates LeetCode problem 645

--- LeetCode/Python/easy/set_mismatch_645.py
+++ LeetCode/Python/easy/set_mismatch_645.py
@@ -6,6 +6,4 @@ https://leetcode.com/problems/set-mismatch/
 
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        missing = list(set(range(1, len(nums) + 1)).difference(nums))[0]
-        double = sum(nums) - sum(set(nums))
-        return [double, missing]
+        return [Counter(nums).most_common(1).pop()[0], set(range(1, len(nums) + 1)).difference(nums).pop()]
