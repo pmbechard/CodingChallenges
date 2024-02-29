6a5dd78397615c25f66aeb441d0ab49bd8d3b7a9
Peyton Bechard <peyton.bechard@gmail.com>
2024-02-29 15:01:26 +0800

    Updates LeetCode problem 1609

--- LeetCode/Python/medium/even_odd_tree_1609.py
+++ LeetCode/Python/medium/even_odd_tree_1609.py
@@ -12,26 +12,15 @@ https://leetcode.com/problems/even-odd-tree
 #         self.right = right
 class Solution:
     def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
-        lvl_map = {}
-        self.traverse(root, 0, lvl_map)
-        i = 0
-        while True:
-            if not lvl_map.get(i):
-                break
-            if i % 2 == 0:
-                if not all([x % 2 == 1 for x in lvl_map[i]]) or not sorted(lvl_map[i]) == lvl_map[i] or not len(
-                        set(lvl_map[i])) == len(lvl_map[i]):
-                    return False
-            else:
-                if not all([x % 2 == 0 for x in lvl_map[i]]) or not sorted(lvl_map[i], reverse=True) == lvl_map[
-                    i] or not len(set(lvl_map[i])) == len(lvl_map[i]):
-                    return False
-            i += 1
-        return True
+        return self.traverse(root, 0, {})
 
     def traverse(self, node, lvl, lvl_map):
+        if lvl % 2 == node.val % 2 or (lvl % 2 == 0 and lvl_map.get(lvl) and lvl_map.get(lvl)[-1] >= node.val) or (
+                lvl % 2 == 1 and lvl_map.get(lvl) and lvl_map.get(lvl)[-1] <= node.val):
+            return False
         lvl_map[lvl] = lvl_map.get(lvl, []) + [node.val]
-        if node.left:
-            self.traverse(node.left, lvl + 1, lvl_map)
-        if node.right:
-            self.traverse(node.right, lvl + 1, lvl_map)
+        if node.left and not self.traverse(node.left, lvl + 1, lvl_map):
+            return False
+        if node.right and not self.traverse(node.right, lvl + 1, lvl_map):
+            return False
+        return True
