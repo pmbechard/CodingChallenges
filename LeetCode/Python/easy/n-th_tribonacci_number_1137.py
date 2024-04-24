bae9af28e6d9dfbb0175fcc32133b61fedd9502f
Peyton Bechard <peyton.bechard@gmail.com>
2024-04-24 08:22:04 +0800

    Updates LeetCode problem 1137

--- LeetCode/Python/easy/n-th_tribonacci_number_1137.py
+++ LeetCode/Python/easy/n-th_tribonacci_number_1137.py
@@ -5,20 +5,13 @@ https://leetcode.com/problems/n-th-tribonacci-number/
 
 
 class Solution:
+    def __init__(self):
+        self.memo = {0: 0, 1: 1, 2: 1}
+
     def tribonacci(self, n: int) -> int:
-        if n == 0 or n == 1:
-            return n
-        elif n == 2:
-            return 1
-
-        a = 0
-        b = 1
-        c = 1
-        d = 0
-
-        for i in range(3, n + 1):
-            d = a + b + c
-            a = b
-            b = c
-            c = d
-        return d
+        if n in self.memo:
+            return self.memo[n]
+        else:
+            result = self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
+            self.memo[n] = result
+            return result
