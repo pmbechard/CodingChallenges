37d0634ac637cb15fa79c663150863aae8db8a7f
Peyton Bechard <peyton.bechard@gmail.com>
2024-05-05 09:22:10 +0800

    Adds LeetCode problem 3131

---
+++ LeetCode/Python/easy/find_the_integer_added_to_the_array_i_3131.py
@@ -0,0 +1,9 @@
+"""
+3131. Find the Integer Added to Array I
+https://leetcode.com/problems/find-the-integer-added-to-array-i
+"""
+
+
+class Solution:
+    def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
+        return min(nums2) - min(nums1)
