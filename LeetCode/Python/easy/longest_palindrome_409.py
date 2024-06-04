f5e575bc24314eb0036a22216bb8495b9173b062
Peyton Bechard <peyton.bechard@gmail.com>
2024-06-04 08:41:30 +0800

    Updates LeetCode problem 409

--- LeetCode/Python/easy/longest_palindrome_409.py
+++ LeetCode/Python/easy/longest_palindrome_409.py
@@ -6,16 +6,14 @@ https://leetcode.com/problems/longest-palindrome/
 
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        count = 0
-        set_s = set(s)
-        has_odd = False
-        for i in set_s:
-            curr_count = s.count(i)
-            if curr_count % 2 == 0:
-                count += curr_count
-            elif not has_odd:
-                count += curr_count
-                has_odd = True
+        odd_set = set()
+        counter = dict()
+        total = 0
+        for c in s:
+            counter[c] = counter.get(c, 0) + 1
+            if counter[c] % 2 == 1:
+                odd_set.add(c)
             else:
-                count += curr_count - 1
-        return count
+                odd_set.remove(c)
+            total += 1
+        return total - len(odd_set) + (1 if len(odd_set) else 0)
