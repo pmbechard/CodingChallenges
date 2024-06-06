c84ff9fcd83fb0bb8f8a55594a78c94185cc6fc2
Peyton Bechard <peyton.bechard@gmail.com>
2024-06-06 09:47:54 +0800

    Updatess LeetCode problem 846

--- LeetCode/Python/medium/hand_of_straights_846.py
+++ LeetCode/Python/medium/hand_of_straights_846.py
@@ -6,19 +6,16 @@ https://leetcode.com/problems/hand-of-straights/
 
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        ctr = Counter(hand)
-        while ctr:
-            if not self.get_straight(ctr, groupSize):
+        hand.sort()
+        while hand:
+            if not self.remove_seq(hand, groupSize):
                 return False
         return True
 
-    def get_straight(self, ctr, groupSize):
-        a = min(ctr)
-        for _ in range(groupSize):
-            if not ctr[a]:
+    def remove_seq(self, hand, groupSize):
+        for card in range(hand[0], hand[0] + groupSize):
+            try:
+                hand.remove(card)
+            except ValueError:
                 return False
-            ctr[a] -= 1
-            if ctr[a] == 0:
-                del ctr[a]
-            a += 1
         return True
