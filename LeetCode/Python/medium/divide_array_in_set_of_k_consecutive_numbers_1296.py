92cdc10a7312e34051d17c68f375a6ec2dad27b0
Peyton Bechard <peyton.bechard@gmail.com>
2024-06-06 10:31:02 +0800

    Updates LeetCode problem 1296

--- LeetCode/Python/medium/divide_array_in_set_of_k_consecutive_numbers_1296.py
+++ LeetCode/Python/medium/divide_array_in_set_of_k_consecutive_numbers_1296.py
@@ -6,11 +6,13 @@ https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/
 
 class Solution:
     def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+        to_del = Counter()
         nums.sort()
-        while nums:
-            for num in range(nums[0], nums[0] + k):
-                try:
-                    nums.remove(num)
-                except ValueError:
-                    return False
-        return True
+        for num in nums:
+            if num in to_del:
+                to_del[num] -= 1
+                if to_del[num] == 0:
+                    del to_del[num]
+            else:
+                to_del.update({i: 1 for i in range(num + 1, num + k)})
+        return False if to_del else True
