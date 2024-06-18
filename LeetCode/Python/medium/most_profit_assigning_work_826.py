4b785a787244470dffb65907e9dc34849df26735
Peyton Bechard <peyton.bechard@gmail.com>
2024-06-18 12:51:25 +0800

    Updates LeetCode problem 826

--- LeetCode/Python/medium/most_profit_assigning_work_826.py
+++ LeetCode/Python/medium/most_profit_assigning_work_826.py
@@ -6,13 +6,12 @@ https://leetcode.com/problems/most-profit-assigning-work/
 
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        total = 0
-        worker.sort(reverse=True)
-        for d, p in sorted(zip(difficulty, profit), key=lambda x: -x[1]):
-            i = 0
-            while i < len(worker) and d <= worker[i]:
+        diff_prof = sorted(zip(difficulty, profit), key=lambda x: -x[1])
+        cntr = Counter(worker)
+        total = i = 0
+        for w in sorted(cntr, reverse=True):
+            while i < len(diff_prof) and w < diff_prof[i][0]:
                 i += 1
-            total += i * p
-            worker = worker[i:]
-            if not worker: break
+            if i < len(diff_prof):
+                total += diff_prof[i][1] * cntr[w]
         return total
