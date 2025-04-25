7ebeae6f6e480a87066698f35bc316d9eced6c6c
Peyton Bechard <peyton.bechard@gmail.com>
2025-04-25 09:48:53 +0800

    Updates LeetCode problem 120

--- LeetCode/Python/medium/triangle_120.py
+++ LeetCode/Python/medium/triangle_120.py
@@ -5,7 +5,7 @@ https://leetcode.com/problems/triangle/
 
 
 class Solution:
-    def minimumTotal(self, triangle: List[List[int]], pos=(0, 0)) -> int:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
         if len(triangle) == 1: return triangle[0][0]
         for row in range(len(triangle) -2, -1, -1):
             for col in range(len(triangle[row])):
@@ -13,3 +13,4 @@ class Solution:
                 right = triangle[row + 1][col + 1] if col + 1 < len(triangle[row + 1]) else None
                 triangle[row][col] += left if right == None else min(left, right)
         return triangle[0][0]
+
