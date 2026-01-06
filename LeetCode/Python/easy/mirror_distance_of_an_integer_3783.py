127d16f19d52ba04999c46bc99d8f9fa77edddbb
Peyton Bechard <peyton.bechard@gmail.com>
2026-01-06 18:34:41 +0800

    Adds LeetCode problem 3783

---
+++ LeetCode/Python/easy/mirror_distance_of_an_integer_3783.py
@@ -0,0 +1,9 @@
+"""
+3783. Mirror Distance of an Integer
+https://leetcode.com/problems/mirror-distance-of-an-integer/
+"""
+
+
+class Solution:
+    def mirrorDistance(self, n: int) -> int:
+        return abs(n - int(str(n)[::-1]))
